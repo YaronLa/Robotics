2739ada37ebbba133db1557a09e1542af3246b78
Oli-nwl <72303772+Oli-nwl@users.noreply.github.com>
2022-10-10 14:05:10 +0200

    Add files via upload

---
+++ drive_middle_gammel.py
@@ -0,0 +1,43 @@
+import numpy as np
+import cv2
+import actions
+import robot
+import slow-selflocalize
+import Exercise3_findthelandmark as cam_imp
+import particles
+
+arlo = robot.Robot()
+camera = cam_imp.CamOpject()
+arucoDict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_6X6_250)
+dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_6X6_250)
+
+def driving_strat(middle, pose):
+    delta_y = middle[1]-pose[1]
+    delta_x = middle[0]-pose[0]
+    dist = np.sqrt(delta_x**2 + delta_y**2)
+    theta = np.arccos(delta_y/dist) 
+    theta_new = (pose[3]-theta)*180/np.pi
+    return theta_new, dist
+
+def drive_to_middle(theta, dist):
+    id_lst = []
+    while len(id_lst) < 2:
+        retval , frameReference = cam.read() # Read frame
+        corners, ids, rejected = cv2.aruco.detectMarkers(frameReference, dict)
+        cv2.aruco.drawDetectedMarkers(frameReference,corners)
+        if not corners:
+            actions.scan_for_object(camera, dict)
+            sleep(1)
+        if corners:
+            if ids in id_lst:
+                continue
+            else:
+                id_lst.append(ids)
+            #arlo.stop()
+            dist, ang_deg, signfunc = actions.detector(corners, markerLength, camera_matrix, dist_coeffs)
+            #actions.drive_to_object(dist, ang_deg, signfunc)
+            sleep(1)
+        sign, theta = np.sign(theta), np.abs(theta)
+        actions.turn(theta, sign)
+        actions.forward_mm(dist)
+        arlo.stop()
