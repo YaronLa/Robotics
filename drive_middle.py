bc86931800f0a2a5a30eca94a6aab49ec56d75eb
flemmingfischer <105373590+flemmingfischer@users.noreply.github.com>
2022-10-10 13:50:38 +0200

    Update drive_middle.py

--- drive_middle.py
+++ drive_middle.py
@@ -5,11 +5,52 @@ import robot
 import CamObjectThread as cam_imp
 import Self_localization_slow as sls
 from time import sleep
+from camera import Camera
 
 arlo = robot.Robot()
 
 #Initilize pipline for cam and spawn cam in seperat thread
-cam_imp.gstreamer_pipeline()
+#Pipline
+def gstreamer_pipeline(capture_width=1280, capture_height=720, framerate=30):
+    """Utility function for setting parameters for the gstreamer camera pipeline"""
+    return (
+        "libcamerasrc !"
+        "videobox autocrop=true !"
+        "video/x-raw, width=(int)%d, height=(int)%d, framerate=(fraction)%d/1 ! "
+        "videoconvert ! "
+        "appsink"
+        % (
+            capture_width,
+            capture_height,
+            framerate,
+        )
+    )
+
+class CamObject:
+    def __init__(self, stream_id=0):
+        #self.cam      = cv2.VideoCapture(stream_id, cv2.CAP_DSHOW)
+        self.cam      = cv2.VideoCapture(gstreamer_pipeline(), apiPreference=cv2.CAP_GSTREAMER)        
+        self.grabbed , self.frame = self.cam.read()
+        self.running = True
+        self.task     = Thread(target=self.update, args=()).start()
+        
+    def update(self):
+       while self.running:
+           self.grabbed , self.frame = self.cam.read()
+       self.cam.release()
+       
+    def read(self):
+        return self.grabbed, self.frame
+    
+    def stop_task(self):
+        self.running = False
+
+
+cam = CamObject() # Cam running in other thread than proces
+sleep(0.5)
+
+#cam_imp.gstreamer_pipeline()
+#cam = camera.Camera(0, 'arlo', useCaptureThread = True)
 #cam = cam_imp.CamObject()
 
 #Defining the aruco dict
